dab0037cc0445ef7e695cbb425ae246719f35b02
Mahdi HamediKhah <m.k.h.13679@gmail.com>
2025-08-07 12:30:35 +0330

    Add new folders from /git directory

---
+++ CodeWars/6kyu/Find The Parity Outlier/findTheParityOutlier.js
@@ -0,0 +1,16 @@
+function findOutlier(integers) {
+  const even = [];
+  const odd = [];
+  integers.forEach((number) => {
+    if (number % 2 == 0) {
+      even.push(number);
+    } else {
+      odd.push(number);
+    }
+  });
+  if (even.length == 1) {
+    return even[0];
+  } else {
+    return odd[0];
+  }
+}
