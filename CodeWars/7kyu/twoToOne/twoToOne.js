f842bf1dad31717aaa0859e9841c8103574b36c7
Mahdi HamediKhah <m.k.h.13679@gmail.com>
2025-08-08 18:05:22 +0330

    ✅ Added: CodeWars/7kyu/twoToOne/twoToOne.js

--- CodeWars/7kyu/twoToOne/twoToOne.js
+++ CodeWars/7kyu/twoToOne/twoToOne.js
@@ -1,1 +1,8 @@
-// Hello Git!
+function longest(s1, s2) {
+  const str = s1 + s2;
+  return str
+    .split("")
+    .sort()
+    .filter((value, i, arr) => arr.indexOf(value) === i)
+    .join("");
+}
