0b3d13b2a9e3080f9c5026a6ffa7e63f13dd7082
Mahdi HamediKhah <m.k.h.13679@gmail.com>
2025-08-10 21:27:07 +0330

    ✅ Added: CodeWars/6kyu/ValidBraces/validBraces.js

---
+++ CodeWars/6kyu/ValidBraces/validBraces.js
@@ -0,0 +1,12 @@
+// function validBraces(braces) {
+//   let result = braces.split("").reduce((acc, cur) => {
+//     cur === "(" ? (acc += 1) : cur === ")" ? (acc -= 1)
+//     : cur === "{" ? (acc += 10) : cur === "}" ? (acc -= 10)
+//     : cur === "[" ? (acc += 100) : cur === "]" ? (acc -= 100);
+//     return acc
+//   }, 0);
+
+//   return result == 0 ? true : false;
+// }
+
+// validBraces("(){}[]");
