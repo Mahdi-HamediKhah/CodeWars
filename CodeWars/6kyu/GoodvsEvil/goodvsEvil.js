d30280532f3f6e89d0bb5139417d177438c2dd4e
Mahdi HamediKhah <m.k.h.13679@gmail.com>
2025-08-14 23:48:28 +0330

    ✅ Added: CodeWars/6kyu/GoodvsEvil/goodvsEvil.js

--- CodeWars/6kyu/GoodvsEvil/goodvsEvil.js
+++ CodeWars/6kyu/GoodvsEvil/goodvsEvil.js
@@ -16,3 +16,13 @@ function goodVsEvil(good, evil) {
     ? "Battle Result: Evil eradicates all trace of Good"
     : "Battle Result: No victor on this battle field";
 }
+
+
+// const goodVsEvil = (g, e) => {
+//   const w = [[1, 2, 3, 3, 4, 10], [1, 2, 2, 2, 3, 5, 10]];
+//   const s = (a, v) => a.split(/\s+/).reduce((t, c, i) => t + c * v[i], 0);
+//   const [G, E] = [s(g, w[0]), s(e, w[1])];
+//   return G > E ? "Battle Result: Good triumphs over Evil" :
+//          G < E ? "Battle Result: Evil eradicates all trace of Good" :
+//                  "Battle Result: No victor on this battle field";
+// };
