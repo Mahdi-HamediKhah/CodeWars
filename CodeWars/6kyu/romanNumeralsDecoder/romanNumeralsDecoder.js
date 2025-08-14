e2e7765b18e90605ab9069ec28af8748b2e9b882
Mahdi HamediKhah <m.k.h.13679@gmail.com>
2025-08-14 23:48:28 +0330

    ✅ Added: CodeWars/6kyu/romanNumeralsDecoder/romanNumeralsDecoder.js

--- CodeWars/6kyu/romanNumeralsDecoder/romanNumeralsDecoder.js
+++ CodeWars/6kyu/romanNumeralsDecoder/romanNumeralsDecoder.js
@@ -19,3 +19,13 @@ function solution(roman) {
   }, 0);
   return result;
 }
+
+
+// function solution(roman) {
+//   const values = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
+//   return [...roman].reduce((sum, cur, i, arr) => {
+//     const curVal = values[cur];
+//     const prevVal = values[arr[i - 1]] || 0;
+//     return sum + (curVal <= prevVal ? curVal : curVal - 2 * prevVal);
+//   }, 0);
+// }
